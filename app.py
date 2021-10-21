3fa90a78402c93b753aa4301a97029a05052d668
r-kyle-kennedy <r.kyle.kennedy@gmail.com>
2021-10-20 19:49:26 -0700

    add_csv function added, duplicate items will only update to last entry

--- app.py
+++ app.py
@@ -14,6 +14,42 @@ def menu():
         return input('\nWhat would you like to do?  ')
 
 
+def clean_price(price_str):
+    price_float = float(price_str[1:])
+    return int((price_float * 1000) / 10)
+
+
+def clean_date(date_str):
+    split_date = date_str.split('/')
+    month = int(split_date[0])
+    day = int(split_date[1])
+    year = int(split_date[2])
+    return datetime.date(year, month, day)
+
+
+def add_csv():
+    with open('inventory.csv') as csvfile:
+        data = csv.reader(csvfile)
+        iter_data = iter(data)
+        next(iter_data)
+        for row in iter_data:
+            product_in_db = session.query(Product).filter(Product.product_name==row[0]).one_or_none()
+            if not product_in_db:
+                name = row[0]
+                price = clean_price(row[1])
+                quantity = int(row[2])
+                date = clean_date(row[3])
+                new_product = Product(product_name=name, product_quantity=quantity, product_price=price, date_updated=date)
+                session.add(new_product)
+            else:
+                item = session.query(Product).filter(Product.product_name==row[0]).first()
+                new_product_date = clean_date(row[3])
+                if item.date_updated < new_product_date:
+                    item.product_price = clean_price(row[1])
+                    item.product_quantity = int(row[2])
+                    item.date_updated = new_product_date
+        session.commit()
+
 
 def app():
     app_running = True
@@ -38,5 +74,5 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    #add_csv()
+    add_csv()
     app()
